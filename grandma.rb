a699b6bd1c942ff695afa7f4ec1bdb147518e735
Deborah Santillan <s.debb@yahoo.com>
2020-05-09 21:47:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ speak = "grandma"
 if speak == "whatever I say"
   puts "HUH?! SPEAK UP, SONNY!"
 end 
-if "CAN YOU HEAR ME?" == "CAN YOU HEAR ME?".lowercase 
+if "CAN YOU HEAR ME?" == "CAN YOU HEAR ME?".downcase 
   return "NO, NOT SINCE 1938!"
 else  
   puts "I LOVE YOU TOO PUMPKIN!"
