32a18499dc204825901bcead1cb19e9d622fe8b8
Deborah Santillan <s.debb@yahoo.com>
2020-05-09 22:06:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,13 +6,7 @@ return "HUH?! SPEAK UP, SONNY!"
 elsif string.upcase == string 
 return "NO, NOT SINCE 1938!"
 end 
-if "CAN YOU HEAR ME?" == "CAN YOU HEAR ME?".downcase 
-  return "NO, NOT SINCE 1938!"
-else  
-  puts "I LOVE YOU TOO PUMPKIN!"
 end
-end
-
 
 
 
